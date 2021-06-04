c874573a72509868fa5da79cc2619f721c4341e5
Christopher Esparza <christopher52@live.com.mx>
2021-06-03 19:42:42 -0500

    correcciones menores

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    document.getElementById("send").style.display = "none";
+   
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
@@ -16,15 +18,39 @@ $(document).ready(function(){
 // typing animation
 
 var typed = new Typed(".typing",{
-    strings: ["Desarrollador","Estudiante","Diseñador","Freelance"],
+    strings: ["Desarrollador","Estudiante","Diseñador","Autodidacta"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
 
 var typed = new Typed(".typing2",{
-    strings: ["Desarrollador","Estudiante","Diseñador","Freelance"],
+    strings: ["Desarrollador","Estudiante","Diseñador","Autodidacta"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
+
+// send email
+
+(function() {
+    emailjs.init("user_HdwPjrBwytKg6Jsd2AgVK");
+    })();
+function sendMail(params){
+    
+    var temParams ={
+        from_name:document.getElementById("from_name").value,
+        asunto:document.getElementById("asunto").value,
+        from_email:document.getElementById("from_email").value,
+        message:document.getElementById("message").value
+        
+    };
+    if(from_name.value != "" && asunto.value != "" && from_email.value != "" && message.value != "" ){
+        emailjs.send('service_0h0dq03','template_wxexrlg',temParams)
+        .then(function(res){
+            console.log("success",res);
+            send = document.getElementById("send").style.display = "block";
+        });
+    }
+    
+}
